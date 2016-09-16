c8972d76864bf3bb742b5d3f932a9f09b7e3cd93
Jia Qian <isjia@163.com>
2016-09-16 22:07:28 +0800

    responsive support for large than 800px

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ $(".footer-fix a").each(function(){
      return false;
    }
  });
+
+$(".sidebar a").each(function(){
+   var path = document.location.pathname.split('/');
+   var page = path[path.length - 1];
+   if (page.length == 0) {
+      page = "index.html";
+   }
+   var href = $(this).attr("href");
+   if (href === page) {
+     $(this).addClass("active");
+     return false;
+   }
+ });
