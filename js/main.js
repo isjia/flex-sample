c99a48148300f24b6609b77c2fdc08d862770f86
Jia Qian <isjia@163.com>
2016-09-16 21:20:21 +0800

    fix active bar display when root router

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(".footer-fix a").each(function(){
    var path = document.location.pathname.split('/');
    var page = path[path.length - 1];
+   if (page.length == 0) {
+      page = "index.html";
+   }
    var href = $(this).attr("href");
    if (href === page) {
      $(this).addClass("active");
